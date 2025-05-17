4c045192b45fb6c73df2706e26210e3d48780cf0
iamkartiksingh-K <kartiksinghk2001@gmail.com>
2025-05-17 19:17:38 +0530

    added home button in auth pages

--- src/app/(auth)/auth/login/page.tsx
+++ src/app/(auth)/auth/login/page.tsx
@@ -1,14 +1,22 @@
 import { LoginCard } from "@/components/login-card";
-import classNames from "classnames";
+import { ArrowLeft } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Page() {
-  const classes = classNames("flex w-full h-dvh items-center");
+  const classes = "flex w-full h-dvh items-center";
   return (
     <div className={classes}>
-      <div className="w-1/2 h-full grow flex justify-center items-center">
+      <div className="w-1/2 h-full grow grid grid-rows-3 grid-cols-3 place-items-center">
+        <Link
+          href="/"
+          className="flex row-start-1 row-end-1 col-start-start-1 col-end-2 place-self-start mt-3 ml-3"
+        >
+          <ArrowLeft className="mr-2" />
+          Home
+        </Link>
         <LoginCard
-          className="shadow-none border-0"
+          className="shadow-none border-0 row-start-2 row-end-3 col-start-2 col-end-3"
           authIconPosition="down"
           externalAuth={true}
         />
--- src/app/(auth)/auth/register/page.tsx
+++ src/app/(auth)/auth/register/page.tsx
@@ -1,12 +1,24 @@
 import { SignupCard } from "@/components/signup-card";
 import classNames from "classnames";
 import Image from "next/image";
+import Link from "next/link";
+import { ArrowLeft } from "lucide-react";
 export default function Page() {
   const classes = classNames("flex w-full h-dvh items-center");
   return (
     <div className={classes}>
-      <div className="w-1/2 h-full grow flex justify-center items-center">
-        <SignupCard className="shadow-none border-0" authIconPosition="down" />
+      <div className="w-1/2 h-full grow grid grid-rows-3 grid-cols-3 place-items-center">
+        <Link
+          href="/"
+          className="flex row-start-1 row-end-1 col-start-start-1 col-end-2 place-self-start mt-3 ml-3"
+        >
+          <ArrowLeft className="mr-2" />
+          Home
+        </Link>
+        <SignupCard
+          className="shadow-none border-0 row-start-2 row-end-3 col-start-2 col-end-3"
+          authIconPosition="down"
+        />
       </div>
       <div className="w-1/2 h-full overflow-hidden hidden md:block">
         <Image
